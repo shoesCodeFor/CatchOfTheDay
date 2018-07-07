4e2f23183f01f708483359c21bdbde08c89c8d33
Schuyler Ankele <schuyler.ankele@gmail.com>
2018-07-07 16:02:49 -0600

    Bounding Box Search

--- assets/javascript/fetchFishData.js
+++ assets/javascript/fetchFishData.js
@@ -6,13 +6,46 @@ var sampleAPICall = 'https://data.ct.gov/resource/mty4-w9bf.json?$query=Select%2
 var fishData;
 
 
-function getData(){
-    fetch(sampleAPICall)
+function getData(URL){
+    fetch(URL)
         .then(function(response) {
             return response.json();
         })
-        .then(function(myJson) {
-            console.log(myJson);
+        .then(function(APIData) {
+            console.log(APIData);
+            fishData = APIData;
+            console.log('.........');
         });
-    }
-getData();
+}
+
+/**
+ * This will create a fish query based on a bounding box.
+ */
+function findFishBB(topLeft, bottomRight){
+    let latRange = [topLeft[0], bottomRight[0]];
+    let lngRange = [topLeft[1], bottomRight[1]];
+    let baseURL = 'https://data.ct.gov/resource/mty4-w9bf.json?$query=Select%20*%20WHERE%20'
+    let conditions = encodeURI(
+        `latitude < ${latRange[0]} AND latitude > ${latRange[1]} AND longitude > ${lngRange[0]} AND longitude < ${lngRange[1]}`);
+    console.log('TEST');
+    console.log(conditions);
+    baseURL+=conditions;
+    console.log(baseURL);
+    getData(baseURL);     
+}
+
+
+
+// Tests
+
+findFishBB([41.907754321521, -73.152064234847], [41.861833, -73.051649]);
+
+// API is Working
+// getData();
+// setTimeout(()=>{
+//     console.log('.........');
+//     console.log(fishData);
+// }, 5000);
+
+
+
