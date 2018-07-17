db6272dca9b8ddf79f2173c220c6549b2ac12f09
Schuyler Ankele <schuyler.ankele@charter.com>
2018-07-16 18:10:26 -0600

    Finished....for now

--- assets/javascript/fetchFishData.js
+++ assets/javascript/fetchFishData.js
@@ -16,6 +16,7 @@ function getData(URL){
             return response.json();
         })
         .then(function(APIData) {
+            fishGroup = [];
             map.removeLayer(fishingSpots);
             console.log(APIData);
             fishData = APIData;
@@ -26,11 +27,34 @@ function getData(URL){
                 L.marker([element.latitude, element.longitude])
                     .bindPopup('<div id="area_id' + element.area_id +'"><a target="_blank" href="' + element.area_link + '"><h3>'+ element.area_name + '</h3></a><h4>'
                         + element.may_catch +'</h4><h4>'+ element.mc2 +'</h4>' +
-                        '<button class="btn btn-success" onclick="getDirections(\'' +
-                        element.latitude.toString() +',' + element.longitude.toString() + '\')">Get Directions</button></div>' + $("<div>").html()
+                        '<button class="btn btn-success" onclick="getDetails(\'' +
+                        element.area_id + '\')">Get Details</button></div>' + $("<div>").html()
                 ).addTo(fishingSpots);
                 let spot;
-                fishGroup.push(element.may_catch);
+                fishGroup.push(element);
+                let listItem = $("<li>");
+                let link  = $('<a>');
+                link.html('<i class="fas fa-plus-circle"></i>');
+                link.on('click', function (){
+                    $('#area' + element.area_id).toggleClass('d-none');
+                });
+                link.prepend(`<h3>${element.area_name}</h3>`);
+                listItem.append(link);
+                listItem.append('<br>');
+                let ul = $('<ul>');
+                ul.attr('id', 'area' + element.area_id);
+                ul.addClass('d-none');
+                ul.prepend('<h5 class="card-title">Most caught fish:</h5>');
+                let innerLi = $('<li>')
+                innerLi.html(element.may_catch);
+                ul.append(innerLi);
+                ul.append(`<br><li>${element.mc2}</li>`);
+                if(element.mc3 !== undefined || element.mc3 !== null){
+                    ul.append(`<br><li>${element.mc3}</li>`);
+                }
+                ul.append(`<br><button class="btn btn-primary" onclick="getDirections('${element.latitude.toString()},${element.longitude.toString()}')">Get Directions</button>`)
+                listItem.append(ul);
+                $('#parkList').append(listItem);
             });
             console.log(fishGroup);
             fishingSpots.addTo(map);
@@ -61,7 +85,14 @@ function findFishByName(typeOfFish){
 
 function getDirections(address){
     console.log(address);
+    L.mapquest.directions().route({
+        start: '41.481350259999999,-72.506571899999997',
+        end: address
+      });
     // L.mapquest.directions();
+}
+function getDetails(loc_id){
+
 }
 
 // Tests
